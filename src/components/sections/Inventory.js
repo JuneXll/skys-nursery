ddecd7f16be9fec5842e21825876ee06af9d4884
JuneXll <71652307+JuneXll@users.noreply.github.com>
2022-01-26 20:57:32 -0500

    Got table to look right

--- src/components/sections/Inventory.js
+++ src/components/sections/Inventory.js
@@ -1,96 +1,85 @@
 import React from 'react';
-import { Container, Table } from 'react-bootstrap';
+// import { Container, Table } from 'react-bootstrap';
 
 const Inventory = () => {
     return(
         <div id="inventory" className='inventory-div cover-container d-flex flex-column justify-content-center align-items-center align-content-center'>
-            <h1>Inventory</h1>
-            <Container className='p-4 table-wrapper-scroll-y my-custom-scrollbar"'>
-                <Table hover striped variant='light'>
+            <h1 className='pb-5'>Inventory</h1>
+            <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                <table class="table table-light table-borderless table-striped table-hover mb-0">
                     <thead>
                         <tr>
-                            <th scope='col'>Plant Name</th>
-                            <th scope='col'>Type</th>
-                            <th scope='col'>Size</th>
+                            <th scope="col">Plant Name</th>
+                            <th scope="col">Size</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <th colspan="3" class="table-active">Hedges</th>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
                         <tr>
-                            <th scope="row">Plant name</th>
-                            <td>Bush</td>
-                            <td>1gallon</td>
+                            <td>Plant Name</td>
+                            <td>Size</td>
                         </tr>
-                        
-                        
+                        <tr>
+                            <td>Plant Name</td>
+                            <td>Size</td>
+                        </tr>
+                        <tr>
+                            <td>Plant Name</td>
+                            <td>Size</td>
+                        </tr>
                     </tbody>
-                </Table>
-
-            </Container>
+                </table>
+            </div>
         </div>
     )
 }
