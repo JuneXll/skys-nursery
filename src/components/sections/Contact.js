a866998e8ba2340bf2358b3c1a92c1998c58d534
JuneXll <71652307+JuneXll@users.noreply.github.com>
2022-01-28 17:12:57 -0500

    Completed layout for delivery areas section and contact us

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import ContactForm from '../widgets/ContactForm';
+
+const plantsImg = 'https://ik.imagekit.io/diversifiedtax/Sky_s_Nursery/MainPlantLineUp.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643405213237'
 
 const Contact = () => {
-    return(
-        <h1> Contact us</h1>
+    return (
+        <div id="contact-us" className='contact-div cover-container d-flex flex-column justify-content-around align-items-center'>
+            {/* Contact us form */}
+            <Container className="row">
+                {/* Contact info */}
+                <Container className='flex-column col-md-6 col-xs-12'>
+                    <div>
+                        <img src={plantsImg} className='plant-lineup-img' alt='plant-line-up'/>
+                    </div>
+                    <Container className='contact-details'>
+                        <h1>Sky's Plant Nursery</h1>
+                        <h2>13120 Sw 202 Ave</h2>
+                        <h2>Miami, FL 33196</h2>
+                        <br/>
+                        <h3>Sergio Lacayo</h3>
+                        <h4>SkysNursery1@gmail.com</h4>
+                        <h4>(786)-238-6737</h4>
+                    </Container>
+                </Container>
+                <Container className='col-md-6 col-xs-12 text-center flex-column my-auto'>
+                    <h1 className='mb-3'>Contact Us</h1>
+                    {/* Contact us form component */}
+                    <ContactForm/>
+                </Container>
+            </Container>
+        </div>
     )
 }
 
